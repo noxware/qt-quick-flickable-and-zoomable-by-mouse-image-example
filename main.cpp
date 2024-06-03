0a082f22bee5c597bb8e2769cfa07a5dd98df7b4
noxware <7684329+noxware@users.noreply.github.com>
2024-06-02 22:34:23 -0300

    nocanstillbb code as an alternative

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    app.setOrganizationDomain("fix");
-    app.setOrganizationName("default file dialog cry");
+    app.setOrganizationDomain("example domain");
+    app.setOrganizationName("example org");
 
     QQmlApplicationEngine engine;
-    //const QUrl url(QStringLiteral("qrc:/main.qml"));
-    const QUrl url(QStringLiteral("qrc:/main_another_way.qml"));
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    // const QUrl url(QStringLiteral("qrc:/alternative/nocanstillbb.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
